5348de1d3a201c58bb32dd0706cdc5f2ca8fe832
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-04-27 19:00:29 +0530

    Add bcrypt dependency to package.json And User Model

---
+++ src/Models/user.model.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongooose'
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+
+userSchema.pre('save',async function ( next ){
+    if(!this.isModified('password'))
+        return next()
+
+    const hashPassword = await bcrypt.hash(this.password, 12);
+    this.password = hashPassword
+    next()
+
+})
+
+
+userSchema.methods.isPasswordCorrect = async function ( password ){
+    return await bcrypt.compare( password, this.password )
+}
+
+export const User = model('User', userSchema )
