801d6c280a2c4cb869cba86507e03e5aab1bf1ae
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-04-28 00:09:25 +0530

    modified user controller

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -1,6 +1,16 @@
 export const Login = async( req, res)=>{
     try {
-        
+        const check = await collection.findOne({ username: req.body.username });
+
+        if (check) {    
+            if (check.password === req.body.password) {
+                res.redirect("https://assetifyy.vercel.app/");
+            } else {
+                res.render("login", { errorMessage: "Invalid username or  password" });
+            }
+        } else {
+            res.render("login", { errorMessage: "Invalid username or password" });
+        }
     } catch (error) {
         res.status(500).json({
             success: false,
@@ -11,6 +21,13 @@ export const Login = async( req, res)=>{
 
 export const Signup = async ( req, res)=>{
     try {
+        const data = {
+            username: req.body.username,
+            password: req.body.password
+        };
+        await collection.insertMany([data]);
+    
+        res.render("login", { errorMessage: undefined });
         
     } catch (error) {
         res.status(500).json({
